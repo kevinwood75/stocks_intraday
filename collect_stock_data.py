77cbd8570c3ad5e54c7421747c0db018a7104dfe
kevinwood75 <kevin.wood75@gmail.com>
2020-04-27 08:07:40 -0400

    test

--- collect_stock_data.py
+++ collect_stock_data.py
@@ -1,16 +1,11 @@
 import json
 import requests
 import datetime
-import pymongo
 import yaml
-mongo_client = pymongo.MongoClient('192.168.2.148', 27017)
-mongo_db = mongo_client['woodez-fi']
-collection_name = 'market_data'
-db_cm = mongo_db[collection_name]
 
-
-def insert_mongo(data):
-    db_cm.insert(data)
+def send_to_api(data):
+    r = requests.post(url = 'http://192.168.2.199:8041/api/fi', data = data)
+    return r.text
 
 def get_quote_data(symbol='msft', data_range='1d', data_interval='10m'):
     res = requests.get('https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?range={data_range}&interval={data_interval}'.format(**locals()))
@@ -22,22 +17,18 @@ def get_quote_data(symbol='msft', data_range='1d', data_interval='10m'):
     datalist = []
     for f, b, g in zip(timelist,closelist,volumelist):
         tmpdict = {
-             "date": datetime.datetime.fromtimestamp(f),
-             "price": b,
-             "volume": g,
-             "ticker": symbol
+             'stock_date': "{0}".format(datetime.datetime.fromtimestamp(f)),
+             'price': b,
+             'volume': g,
+             'ticker': symbol
         }
-        
-        insert_mongo(tmpdict)
-      
+        json_data = json.dumps(tmpdict)        
+        output = send_to_api(json_data)
+        return output 
 
 if __name__ == "__main__":
    with open('stocks_collect.yml') as file:
        conf = yaml.load(file)
 
-#   stocks = ['SQ','MSFT','TWLO','SPLK','FB','SNAP','NPI.TO','BYL.TO','NOK','TSLA','GILD']
    for ticker in conf['tickers']:
-       get_quote_data(ticker, '5d', '1m')
-#   cursor = db_cm.find({})
-#   for document in cursor:
-#       print(document)
+       get_quote_data(ticker, '1d', '1m')
